123fbe62bd986c08e1970734913e0f6dd0f14841
a20l <aidaytyn@gmail.com>
2021-07-30 15:23:42 -0500

    First commit

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDx8e6QbXqgQcPWkG2XLW6R36Jal1NmFtU",
+    authDomain: "clone-8b3be.firebaseapp.com",
+    projectId: "clone-8b3be",
+    storageBucket: "clone-8b3be.appspot.com",
+    messagingSenderId: "124092095548",
+    appId: "1:124092095548:web:e9b0610ea0f8147b2e7739",
+    measurementId: "G-RS4BEV5L24"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
